8015d31290e577ae0efa6fbf17d4d5ed7757af5d
ming25 <xiangming25@163.com>
2016-11-25 09:06:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    'ACCESS_KEY': 'opOqcJMYkPEaqqFjRw3umwdljqTW8F6QaISqG4N8',
-    'SECRET_KEY': '39XsWPfCfaSvGK5kzNOilwJueLN0HJnDHVAF0LPD',
-    'Bucket_Name': 'pictures',
+    'ACCESS_KEY': '<ACCESS_KEY>',
+    'SECRET_KEY': '<SECRET_KEY>',
+    'Bucket_Name': '<Bucket_Name>',
     'Port': 3000,
     'Uptoken_Url': 'uptoken',
-    // 'Domain': 'http://qiniu-plupload.qiniudn.com/',
-    'Domain':'http://oh1bwget0.bkt.clouddn.com/'
+    'Domain': 'http://qiniu-plupload.qiniudn.com/'
 };
